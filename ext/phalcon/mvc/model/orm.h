f61b21e852720a69c6cea33492d75bdbb04351e3
Mikhail Snetkov <ufoproger@gmail.com>
2021-01-18 16:59:35 +0700

    Remove broken macros from orm.h

--- ext/phalcon/mvc/model/orm.h
+++ ext/phalcon/mvc/model/orm.h
@@ -17,5 +17,5 @@
   +------------------------------------------------------------------------+
 */
 
-void phalcon_orm_destroy_cache(TSRMLS_D);
-void phalcon_orm_singlequotes(zval *return_value, zval *str TSRMLS_DC);
+void phalcon_orm_destroy_cache();
+void phalcon_orm_singlequotes(zval *return_value, zval *str);
